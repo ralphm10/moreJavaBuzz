92bc3aefde3e480ef4288abe2d35843d252d77a0
Ralph Mallett <rmallett12@gmail.com>
2021-01-07 12:39:55 +0000

    Test: return Buzz if divisible by 5

--- src/JavaBuzz.java
+++ src/JavaBuzz.java
@@ -2,6 +2,8 @@ public class JavaBuzz {
     public String checkNumber(int num) {
         if (num % 3 == 0) {
             return "Fizz";
+        } else if (num % 5 == 0) {
+            return "Buzz";
         } else {
             return String.valueOf(num);
         }
--- src/JavaBuzzTest.java
+++ src/JavaBuzzTest.java
@@ -8,13 +8,20 @@ public class JavaBuzzTest {
     public void returnsNumber() {
         JavaBuzz javabuzz = new JavaBuzz();
         String result = javabuzz.checkNumber(1);
-        assertEquals(result, "1");
+        assertEquals("1", result);
     }
 
     @Test
     public void returnsFizzIfDivisibleByThree() {
         JavaBuzz javabuzz = new JavaBuzz();
         String result = javabuzz.checkNumber(3);
-        assertEquals(result, "Fizz");
+        assertEquals("Fizz", result);
+    }
+
+    @Test
+    public void returnsBuzzIfDivisibleByFive() {
+        JavaBuzz javabuzz = new JavaBuzz();
+        String result = javabuzz.checkNumber(5);
+        assertEquals("Buzz", result);
     }
 }
